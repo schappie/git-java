d35538d1d9aec3f67af76ea432132e60d684c8f2
Sawyer Chapman <schapman1@stu.jsu.edu>
2022-08-31 19:33:02 -0500

    Added f2()

--- Class1.java
+++ Class1.java
@@ -2,4 +2,7 @@ public class Class1 {
 	private void f1() {
 		//Placeholder meathod (v1.0)
 	}
+	private void f2() {
+		//Placeholder meathod (v2.0)
+	}
 }
--- Class2.java
+++ Class2.java
@@ -2,4 +2,7 @@ public class Class2 {
 	private void f1() {
 		//Placeholder meathod (v1.0)
 	}
+	private void f2() {
+		//Placeholder meathod (v2.0)
+	}
 }
